c34f99cb3610e398f72cebb7fca459171c8d230d
Luan Miqueias <luanmiqueias92@gmail.com>
2020-12-04 15:26:29 -0300

    Criando Landing Page

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react'
+import './app.css';
+import Landing from './pages/landingPage/landing';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Landing/>
     </div>
   );
 }
