9a2579fb3a260c419d2ddff663112b0cd330d766
Luan Miqueias <luanmiqueias92@gmail.com>
2020-12-17 03:27:01 -0300

    Integrando listagem com API

--- app/src/components/QuizItem/quizItem.js
+++ app/src/components/QuizItem/quizItem.js
@@ -1,35 +1,48 @@
-import React from 'react'
-import './style.css'
-import iconHeart from '../../assets/icons/IconHeart.svg';
-import iconHeartSelecionado from '../../assets/icons/IconHeart-selecionado.svg';
+import React from "react";
+import "./style.css";
+import iconHeart from "../../assets/icons/IconHeart.svg";
+import iconHeartSelecionado from "../../assets/icons/IconHeart-selecionado.svg";
+import { Link } from "react-router-dom";
 
-function QuizItem({title, description, tags, favorited}){
-    const [isFavorite, SetIsFavorite] = React.useState()
-    
-    React.useEffect(() => {
-        if(favorited){
-            SetIsFavorite(() => <div className="favorited"><img src={iconHeartSelecionado}alt="remover dos avorito"/></div>)
-        }else{
-            SetIsFavorite(() => <div className="favorited"><img src={iconHeart}alt="adicionar aos favorito"/></div>)
-        }
-    }, [SetIsFavorite, favorited])
-    return(
-        <div className="container-quizItem">
-            {isFavorite}
-            <div className="content-quizItem">
-                <div>
-                <h1>{title}</h1>
-                <p>{description}</p>
-                </div>
-                <div className="container-spanItems">
-                    {tags.map((item) => <span key={item} className="tag">{item}</span>)}
-                </div>
-            </div>
-            <div className="buttonBlock">
-                <button  className="btn">Responder</button>
-            </div>
+function QuizItem({ title, description, tags, favorited, id }) {
+  const [isFavorite, SetIsFavorite] = React.useState();
+
+  React.useEffect(() => {
+    if (favorited) {
+      SetIsFavorite(() => (
+        <div className="favorited">
+          <img src={iconHeartSelecionado} alt="remover dos favoritos" />
         </div>
-    );
+      ));
+    } else {
+      SetIsFavorite(() => (
+        <div className="favorited">
+          <img src={iconHeart} alt="adicionar aos favoritos" />
+        </div>
+      ));
+    }
+  }, [SetIsFavorite, favorited]);
+  return (
+    <div className="container-quizItem">
+      {isFavorite}
+      <Link to={"quiz/" + id} className="content-quizItem">
+        <div>
+          <h1>{title}</h1>
+          <p>{description}</p>
+        </div>
+        <div className="container-spanItems">
+          {tags.map((item) => (
+            <span key={item} className="tag">
+              {item}
+            </span>
+          ))}
+        </div>
+      </Link>
+      <Link to={"quiz/" + id} className="buttonBlock">
+        <button className="btn">Responder</button>
+      </Link>
+    </div>
+  );
 }
 
-export default QuizItem
+export default QuizItem;
--- app/src/pages/Routes.js
+++ app/src/pages/Routes.js
@@ -1,15 +1,17 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import Landing from './Landing/landing';
-import QuizList from './QuizList/QuizList'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Landing from "./Landing/landing";
+import Quiz from "./Quiz/Quiz";
+import QuizList from "./QuizList/QuizList";
 function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact={true} component={Landing} />
-                <Route path="/all" component={QuizList} />
-            </Switch>
-        </BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={Landing} />
+        <Route path="/all" component={QuizList} />
+        <Route path="/quiz/:id" component={Quiz} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
-export default Routes
+export default Routes;
