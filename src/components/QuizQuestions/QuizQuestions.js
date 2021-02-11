2bba8aa7ef8254c4afc1547f60aea720ffa1f8e7
Luan Miqueias <luanmiqueias92@gmail.com>
2021-02-10 21:13:24 -0300

    Função para somar acertos

--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "./style.css";
 
-
-function QuizQuestions({ pergunta, index }) {
+function QuizQuestions({
+  pergunta,
+  indexPergunta,
+  setRespostas,
+  respostas,
+  onChange,
+}) {
   // console.log(pergunta);
   let indexAlternativa = 0;
   function letraAlternativa(index) {
     const letraArray = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-    indexAlternativa = indexAlternativa + 1;    
+    indexAlternativa = indexAlternativa + 1;
     return letraArray[index];
   }
-  function randomNumber(){
+  function randomNumber() {
     return 1;
     // return Math.floor(Math.random() * (pergunta.alternativas.length - 1));
   }
@@ -20,13 +25,20 @@ function QuizQuestions({ pergunta, index }) {
       <h1>{pergunta.pergunta}</h1>
       <div className="content-pergunta">
         {pergunta.alternativas.map((alternativa, index) => {
-          
           return (
-            <label htmlFor={"alternativa_" + index} key={index} style={{order: randomNumber()}} className="alternativa">
+            <label
+              htmlFor={"alternativa_" + index}
+              key={index}
+              style={{ order: randomNumber() }}
+              className="alternativa"
+            >
               <input
                 type="radio"
                 name="alternativa"
                 id={"alternativa_" + index}
+                value={alternativa}
+                onChange={onChange}
+                checked={respostas["pergunta" + indexPergunta] === alternativa}
               />
               <span>{letraAlternativa(indexAlternativa)}</span>
               <p>{alternativa}</p>
--- src/pages/Quiz/Quiz.js
+++ src/pages/Quiz/Quiz.js
@@ -11,19 +11,80 @@ import QuizQuestions from "../../components/QuizQuestions/QuizQuestions";
 function Quiz() {
   const [pergunta, setPergunta] = React.useState(null);
   const [start, setStart] = React.useState(false);
-  const [indexPergunta, setIndexPergunta] = React.useState(0);
+  const [resultado, setResultado] = React.useState(false);
+  const [acertos, setAcertos] = React.useState(0);
 
+  const [perguntasLength, setPerguntasLength] = React.useState(0);
+
+  const [indexPergunta, setIndexPergunta] = React.useState(0);
+  const [respostas, setRespostas] = React.useState({ pergunta0: {} });
   const params = useParams();
+
+  function handleChange({ target }) {
+    setRespostas({
+      ...respostas,
+      ["pergunta" + indexPergunta]: target.value,
+    });
+  }
   React.useEffect(() => {
     fetch("https://quizluan.herokuapp.com/quiz/" + params.id)
       .then((responce) => responce.json())
       .then((json) => {
         console.log(json);
         setPergunta(json);
+        setPerguntasLength(Object.keys(json.perguntas[0]).length);
       });
   }, [setPergunta, params]);
+  function handleProximaPergunta(e) {
+    if (indexPergunta === perguntasLength - 1) {
+      setStart(false);
+      verAcertos();
+      return setResultado(true);
+    } else {
+      setIndexPergunta((ant) => ++ant);
+    }
+  }
+  function verAcertos() {
+    const totalPerguntas = perguntasLength;
+    const respostasPerguntas = resultado;
+
+    let arrayPerguntas = [];
+    // const teste = ;
+    for (let i = 1; i <= totalPerguntas; i++) {
+      arrayPerguntas = [
+        ...arrayPerguntas,
+        pergunta.perguntas[0]["pergunta" + i],
+      ];
+    }
+    const corretas = arrayPerguntas.filter(({ correta }, index) => {
+      return respostas["pergunta" + index] === correta;
+    });
+    console.log(corretas.length);
+    const acertosPorcentagem = (100 / totalPerguntas) * corretas.length;
+    setAcertos(acertosPorcentagem);
+    console.log(corretas);
+  }
+  if (resultado) {
+    return (
+      <div className="quiz resultado">
+        <main>
+          <div className="content">
+            <div className="content-resultado">
+              <h1>Você acertou:</h1>
+              {acertos > 50 ? (
+                <h2 className="resultadoBom">{acertos}%</h2>
+              ) : (
+                <h2 className="resultadoRuim">{acertos}%</h2>
+              )}
+              <Link to="/all">Voltar</Link>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
   if (pergunta) {
-    console.log(pergunta);
+    // console.log(respostas);
     return (
       <div className="quiz">
         <main>
@@ -43,14 +104,16 @@ function Quiz() {
               <div className="pergunta">
                 <QuizQuestions
                   pergunta={
-                    
                     pergunta.perguntas[0]["pergunta" + (indexPergunta + 1)]
                   }
-                  index={indexPergunta}
+                  indexPergunta={indexPergunta}
+                  respostas={respostas}
+                  onChange={(e) => handleChange(e)}
+                  checked={respostas}
                 />
                 <button
                   className="btnProxima"
-                  onClick={() => setIndexPergunta((ant) => ++ant)}
+                  onClick={(e) => handleProximaPergunta(e)}
                 >
                   Proxima
                 </button>
