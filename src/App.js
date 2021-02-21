a74c25aee2a1a4d163e2ff7ef01132095f401e65
Luan Miqueias <luanmiqueias92@gmail.com>
2021-02-20 22:59:18 -0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import Routes from './pages/Routes';
-import { GlobalStorage } from './pages/GlobalStorage';
+import React from "react";
+import Routes from "./pages/Routes";
+import { GlobalStorage } from "./pages/GlobalStorage";
 function App() {
   return (
     <GlobalStorage>
